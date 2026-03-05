687a37df5071716dcfa7be6e88b2373233be52b3
Lagouit <noureddinelagouit@gmail.com>
2026-03-05 16:50:30 +0000

    test(infra): ajout des tests pour valider le Singleton BDD et les Factories

---
+++ tests/test_infrastructure.py
@@ -0,0 +1,18 @@
+﻿# tests/test_infrastructure.py
+from infrastructure.database.gestionnaire_bdd import GestionnaireBDD
+from infrastructure.factories.fabrique_utilisateur import FabriqueUtilisateur
+from core.enums.type_utilisateur import TypeUtilisateur
+from core.entities.fournisseur import Fournisseur
+
+def test_singleton_bdd():
+    """Vérifie que le Singleton garantit une instance unique (p. 211)."""
+    db1 = GestionnaireBDD()
+    db2 = GestionnaireBDD()
+    assert db1 is db2
+
+def test_factory_fournisseur():
+    """Vérifie que la Factory crée la bonne sous-classe (p. 214)."""
+    data = {"id": 1, "nom": "Noureddine", "email": "n@chef.ma", "mot_de_passe": "123"}
+    user = FabriqueUtilisateur.creer_utilisateur(TypeUtilisateur.FOURNISSEUR, data)
+    assert isinstance(user, Fournisseur)
+    assert user.type_utilisateur == TypeUtilisateur.FOURNISSEUR
